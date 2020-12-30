44495df8e63f394ae659773dbcd44f496421e963
Hoopes <matthew.hoopes@gmail.com>
2020-12-30 14:20:13 -0500

    Initial Commit

---
+++ RouteTest/RouteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteTestApp.swift
+//  RouteTest
+//
+//  Created by Matthew Hoopes on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct RouteTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
